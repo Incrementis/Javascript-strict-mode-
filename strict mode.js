3498b3fb578e546bb4a849023718ae9944439778
Akin C <no-budget-games@outlook.de>
2017-01-20 20:27:46 +0100

    First upload of strict mode testing
    Contains all the files needed to test javascript strict mode.

---
+++ strict mode.js
@@ -0,0 +1,12 @@
+//Just a basic function
+function strict_mode_example()
+{
+	//Activating the mode!
+	"use strict";
+	
+	//Gives an error in "strict mode" - to understand "arguments" check:
+	//http://www.javascriptkit.com/javatutors/arrayprototypeslice.shtml
+	var arguments = [];
+	
+	alert(list);
+}
